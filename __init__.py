f70e52af35634d3445b6d9d24c8aa261c66c781a
tsuyuki makoto <mtsuyuki@gmail.com>
2012-10-24 21:36:29 +0900

    すぐに忘れるので書いておいた

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+from easy_mail import send_mail
+
+__all__ = ['send_mail',]
---
+++ easy_mail.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+
+import smtplib
+from email.MIMEText import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.Header import Header
+from email.Utils import formatdate
+from email import Encoders
+
+def send_mail(sender, sender_label, receivers, subject, body, charset, attachment=None, filename=None):
+    SMTP_SERVER = ('127.0.0.1', 25)
+
+    msg = None
+    if attachment:
+        msg = MIMEMultipart()
+        file_part = MIMEBase('application', "octet-stream")
+        file_part.set_payload( attachment.read() )
+        Encoders.encode_base64(file_part)
+        file_part.add_header('Content-Disposition', 'attachment; filename="%s"' % filename)
+        body_part = MIMEText(body.encode(charset), 'plain', charset)
+        msg.attach(file_part)
+        msg.attach(body_part)
+    else:
+        # 'text/plain; charset="encoding"'というMIME文書を作ります
+        msg = MIMEText(body.encode(charset), 'plain', charset)
+
+    if sender_label:
+        msg['From'] = u"%s <%s>" % (str(Header(sender_name, charset)), sender)
+    else:
+        msg['Form'] = sender
+
+    msg['Subject'] = Header(subject.encode(charset), charset)
+    msg['To'] = ','.join(receivers)
+    msg['Date'] = formatdate()
+    s = smtplib.SMTP(*SMTP_SERVER)
+    s.sendmail(sender, receivers, msg.as_string())
+    s.close()
+
+if __name__ == '__main__':
+    from StringIO import StringIO
+    csvfile = StringIO('''123,456\n789,101\n111,112''')
+    #受信者(リストで複数指定)
+    receivers = ['mtsuyuki@gmail.com',]
+    #メーラーが表示する送信者
+    sender_name = u"露木 誠"
+    #件名
+    subject = u"メール送信のテスト"
+    #本文
+    body = u"""日本語の本文です。
+    なんか書いたりする"""
+    send_mail('tsuyuki@tsuyukimakoto.com', None, receivers, subject, body, 'ISO-2022-JP')#, csvfile, '20121024.csv')
